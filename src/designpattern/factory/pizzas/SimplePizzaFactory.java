95c607e44c9a955b9f081556c3bdcfa6c846d653
SungRyeolKim <kimsr0429@inha.edu>
2024-04-12 23:03:04 +0900

    factory - simple pizza

---
+++ src/designpattern/factory/pizzas/SimplePizzaFactory.java
@@ -0,0 +1,14 @@
+package designpattern.factory.pizzas;
+
+public class SimplePizzaFactory {
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+
+        if(type.equals("cheese")) pizza = new CheesePizza();
+        else if(type.equals("pepperoni")) pizza = new PepperoniPizza();
+        else if(type.equals("calm")) pizza = new ClamPizza();
+        else if(type.equals("veggie")) pizza = new VeggiePizza();
+
+        return pizza;
+    }
+}
