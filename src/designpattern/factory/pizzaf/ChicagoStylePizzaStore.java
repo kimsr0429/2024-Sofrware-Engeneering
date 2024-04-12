13ed4eb8c8a230090fbdce7875a6f45c3e685b88
SungRyeolKim <kimsr0429@inha.edu>
2024-04-12 23:56:38 +0900

    factory - abstract factory pizza

---
+++ src/designpattern/factory/pizzaf/ChicagoStylePizzaStore.java
@@ -0,0 +1,12 @@
+package designpattern.factory.pizzaf;
+
+public class ChicagoStylePizzaStore extends PizzaStore {
+    @Override
+    Pizza createPizza(String item) {
+        if(item.equals("cheese")) return new ChicagoStyleCheesePizza();
+        else if(item.equals("pepperoni")) return new ChicagoStylePepperoniPizza();
+        else if(item.equals("clam")) return new ChicagoStyleClamPizza();
+        else if(item.equals("veggie")) return new ChicagoStyleCheesePizza();
+        else return null;
+    }
+}
