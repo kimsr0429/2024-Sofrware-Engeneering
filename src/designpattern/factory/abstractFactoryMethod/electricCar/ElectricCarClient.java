401182fee3eb0887592b966268783f97397ca445
SungRyeolKim <kimsr0429@inha.edu>
2024-04-26 23:37:37 +0900

    singleton pattern

--- src/designpattern/factory/abstractFactoryMethod/electricCar/ElectricCarClient.java
+++ src/designpattern/factory/abstractFactoryMethod/electricCar/ElectricCarClient.java
@@ -2,18 +2,18 @@ package designpattern.factory.abstractFactoryMethod.electricCar;
 
 public class ElectricCarClient {
     public static void main(String[] args) {
-        CarFactory factory1 = new TeslaFactory();
-        ElectricMotor motor1 = factory1.createMotor();
-        Battery battery1 = factory1.createBattery();
-
-        motor1.start();
-        System.out.println("테슬라 배터리 1회 충전 거리 : " + battery1.getRange() + "km");
-
-        CarFactory factory2 = new HyundaiFactory();
-        ElectricMotor motor2 = factory2.createMotor();
-        Battery battery2 = factory2.createBattery();
-
-        motor2.start();
-        System.out.println("현대차 배터리 1회 충전 거리 : " + battery2.getRange() + "km");
+//        CarFactory factory1 = new TeslaFactory();
+//        ElectricMotor motor1 = factory1.createMotor();
+//        Battery battery1 = factory1.createBattery();
+//
+//        motor1.start();
+//        System.out.println("테슬라 배터리 1회 충전 거리 : " + battery1.getRange() + "km");
+//
+//        CarFactory factory2 = new HyundaiFactory();
+//        ElectricMotor motor2 = factory2.createMotor();
+//        Battery battery2 = factory2.createBattery();
+//
+//        motor2.start();
+//        System.out.println("현대차 배터리 1회 충전 거리 : " + battery2.getRange() + "km");
     }
 }
---
+++ src/designpattern/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package designpattern.singleton;
+
+public class Singleton {
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+}
